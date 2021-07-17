471a05830c462bb794cab43392403b443874184e
Atmagyan Singh <55326048+Atmagyan@users.noreply.github.com>
2021-07-17 11:12:13 +0530

    File via upload

---
+++ src/contacts.js
@@ -0,0 +1,26 @@
+const contacts = [
+  {
+    id: 1,
+    name: "Beyonce",
+    imgURL:
+      "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+    phone: "+123 456 789",
+    email: "b@beyonce.com"
+  },
+  {
+    id: 2,
+    name: "Kajal",
+    imgURL: "https://www.businessupturn.com/wp-content/uploads/2020/09/kajal-aggarwal-flaunts-her-sexy-curves-in-this-picture-201701-1483704735-300x300-1.jpg",
+    phone: "+987 654 321",
+    email: "kajal@gmail.com"
+  },
+  {
+    id: 3,
+    name: "Tapsee",
+    imgURL: "https://tse4.mm.bing.net/th/id/OIP.MNcYYIK-sdvYU-U8-15edgHaGL?w=206&h=172&c=7&o=5&pid=1.7",
+    phone: "+918 372 574",
+    email: "pannutapsee@gmail.com"
+  }
+];
+
+export default contacts;
